80fa2aae0b87da9308062ed3c4ad098d35585d62
Kariuki11 <irungukenneth433@gmail.com>
2024-07-16 21:57:08 +0300

    New changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import React, {FC, useState } from 'react';
+import React, {FC, ChangeEvent, useState } from 'react';
 import './App.css';
 
 const App: FC = () => {
 
   const [task, setTask] = useState<string>("");
   const [deadline, setDeadline] = useState<number>(0);
-  const [todo, setTodoList] = useState<string>("")
+  const [todo, setTodoList] = useState([]);
 
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTask(event.target.value)
+  };
   return (
   <div className="App">
     <div className="header">
       <div className="inputContainer">
-      <input type="text" placeholder="Task..." />
+      <input type="text" placeholder="Task..." name="task" onChange={handleChange} />
       <input type="number" placeholder="Deadline(in days)..." />
       </div>
       <button>ADD Task</button>
